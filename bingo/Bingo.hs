cca3075f72514c0b6638c011dc1005353a6a74b1
Jakub Järvenpää <jakub.jarvenpaa@reaktor.fi>
2011-12-21 20:33:39 +0200

    First bingo doodles

---
+++ bingo/Bingo.hs
@@ -0,0 +1,18 @@
+import System.Environment
+import System.Random
+import System.Random.Shuffle
+import Data.List
+import Control.Monad
+
+main = do file <- fmap head getArgs
+          items <- getLines file 
+          seed  <- newStdGen
+          let shuffledItems = shuffle' items (length items) seed
+          print $ bingo 3 shuffledItems
+
+getLines = liftM lines . readFile
+
+bingo size = take size . chunk size . take (size * size)
+
+chunk _ [] = [[]]
+chunk n xs = y1 : chunk n y2 where (y1, y2) = splitAt n xs
